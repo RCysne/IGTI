91257ecfce11fa07c60638104f2c15c55b0675f5
Ronaldo Cysne <ronaldocysne@gmail.com>
2021-07-18 13:32:55 -0300

    Sprint 3 do Projeto Aplicado
    Criação do formulário com abas para os clientes e parceiros

--- PA/src/App.js
+++ PA/src/App.js
@@ -1,20 +1,20 @@
-import { render } from '@testing-library/react';
-import React from 'react';
-import Header from '/home/cysne/React/igti-pa/src/Components/header/header';
-import Section from '/home/cysne/React/igti-pa/src/Components/section/section';
-import Form from '/home/cysne/React/igti-pa/src/Components/form/form';
-import './style.css';
+import { render } from '@testing-library/react'
+import React from 'react'
+import Header from '/home/cysne/React/igti-pa/src/Components/header/header'
+import Section from '/home/cysne/React/igti-pa/src/Components/section/section'
+import Form from '/home/cysne/React/igti-pa/src/Components/form/formAbas'
+import './style.css'
 
 const App = () => {
- render(); {
-  return (
-    <div className="App">
-      <Header/>
-      <Section/>
-      <Form/>
-    </div>
-    );
-
+  render()
+  {
+    return (
+      <div className="App">
+        <Header />
+        <Section />
+        <Form />
+      </div>
+    )
+  }
 }
-}
-export default App;
+export default App
--- PA/src/Components/form/form.js
+++ PA/src/Components/form/form.js
@@ -1,65 +1,204 @@
-import React, { Component } from 'react';
-import './form.css';
-
+import React, { Component } from 'react'
+import './form.css'
 
 class form extends React.Component {
-    render() {
-        return(
+  render() {
+    return (
+      // <div className="form">
+      //   <div className="cadastramento">
+      //     <p>Cadastro</p>
+      //   </div>
 
-    <div className="form">
+      //   <div className="logoRegister">
+      //     <div className="white">
+      //       <img
+      //         id="logoRegister"
+      //         src="./Images/logoDeitada.png"
+      //         alt="Logomarca Luciana Otoch"
+      //       />
+      //     </div>
+      //   </div>
 
+      //   <section className="sectionForm" id="sectionForm">
+      //     <form className="formulario" action="">
+      //       <div className="partner">
+      //         <div className="namePartner">
+      //           <label htmlFor="nomeParceiro">Nome: </label>
+      //           <input
+      //             type="text"
+      //             name="nomeParceiro"
+      //             id="nomeParceiro"
+      //             placeholder="Digite o seu nome."
+      //           />
+      //         </div>
+
+      //         <div className="nameBusiness">
+      //           <label htmlFor="nomeEmpresa">Empresa: </label>
+      //           <input
+      //             type="text"
+      //             name="nomeEmpresa"
+      //             id="nomeEmpresa"
+      //             placeholder="Digite o nome da empresa."
+      //           />
+      //         </div>
+
+      //         <div className="emailPartner">
+      //           <label htmlFor="emailParceiro">Email: </label>
+      //           <input
+      //             type="email"
+      //             name="emailParceiro"
+      //             id="emailParceiro"
+      //             placeholder="Digite o seu email."
+      //           />
+      //         </div>
+      //       </div>
+
+      //       <input
+      //         className="btn"
+      //         type="button"
+      //         value="Cadastrar"
+      //         id="btnRegister"
+      //       />
+
+      //       <div className="login-data">
+      //         <div className="login">
+      //           <label htmlFor="loginParceiro">Digite seu login: </label>
+      //           <input
+      //             type="text"
+      //             name="loginRegister"
+      //             id="loginRegister"
+      //             placeholder="Login"
+      //           />
+      //         </div>
+
+      //         <div className="password-data">
+      //           <label htmlFor="senhaParceiro">Digite sua senha: </label>
+      //           <input
+      //             type="password"
+      //             name="senhaParceiro"
+      //             id="senhaParceiro"
+      //             placeholder="Senha"
+      //           />
+      //         </div>
+      //       </div>
+      //       <input className="btn" type="button" value="Entrar" id="btnLogin" />
+      //     </form>
+      //   </section>
+      // </div>
+
+      <div className="form">
         <div className="cadastramento">
-            <p>Cadastro</p>
+          <p>Cadastro</p>
         </div>
 
         <div className="logoRegister">
-            <div className="white">
-                <img id="logoRegister" src="./Images/logoDeitada.png" alt="Logomarca Luciana Otoch"/>
-            </div>
+          <div className="white">
+            <img
+              id="logoRegister"
+              src="./Images/logoDeitada.png"
+              alt="Logomarca Luciana Otoch"
+            />
+          </div>
         </div>
 
         <section className="sectionForm" id="sectionForm">
-    
-            <form className="formulario" action="">
-
-                <div className="partner">
-                    <div className="namePartner">
+          <form className="formulario" action="">
+            <nav className="nav-tabs">
+              <ul>
+                <li>
+                  <input
+                    type="radio"
+                    name="tabs"
+                    className="rd_tabs"
+                    id="tab1"
+                    checked
+                  />
+                  <label className="labelF" htmlFor="tab1">
+                    Clientes
+                  </label>
+                  <div className="content">
+                    <div className="login-data">
+                      <div className="login">
+                        <label htmlFor="loginParceiro">
+                          Digite seu login:{' '}
+                        </label>
+                        <input
+                          type="text"
+                          name="loginRegister"
+                          id="loginRegister"
+                          placeholder="Login"
+                        />
+                      </div>
+                      <div className="password-data">
+                        <label htmlFor="senhaParceiro">
+                          Digite sua senha:{' '}
+                        </label>
+                        <input
+                          type="password"
+                          name="senhaParceiro"
+                          id="senhaParceiro"
+                          placeholder="Senha"
+                        />
+                      </div>
+                      <button className="btn" id="btn_client">
+                        Enviar
+                      </button>
+                    </div>
+                  </div>
+                </li>
+                <li>
+                  <input
+                    type="radio"
+                    name="tabs"
+                    className="rd_tabs"
+                    id="tab2"
+                  />
+                  <label className="labelF" htmlFor="tab2">
+                    Parceiros
+                  </label>
+                  <div className="content">
+                    <div className="partner">
+                      <div className="namePartner">
                         <label htmlFor="nomeParceiro">Nome: </label>
-                        <input type="text" name="nomeParceiro" id="nomeParceiro" placeholder="Digite o seu nome."/>
+                        <input
+                          type="text"
+                          name="nomeParceiro"
+                          id="nomeParceiro"
+                          placeholder="Digite o seu nome."
+                        />
+                      </div>
+                      <div className="nameBusiness">
+                        <label htmlFor="nomeEmpresa">Empresa: </label>
+                        <input
+                          type="text"
+                          name="nomeEmpresa"
+                          id="nomeEmpresa"
+                          placeholder="Digite o nome da empresa."
+                        />
+                      </div>
+                      <div className="emailPartner">
+                        <label htmlFor="emailParceiro">Email: </label>
+                        <input
+                          type="email"
+                          name="emailParceiro"
+                          id="emailParceiro"
+                          placeholder="Digite o email cadastrado."
+                        />
+                      </div>
+
+                      <button className="btn" id="btn_partner">
+                        Enviar
+                      </button>
                     </div>
-
-                <div className="nameBusiness">
-                    <label htmlFor="nomeEmpresa">Empresa: </label>
-                    <input type="text" name="nomeEmpresa" id="nomeEmpresa" placeholder="Digite o nome da empresa."/>
-                </div>
-
-                <div className="emailPartner">
-                    <label htmlFor="emailParceiro">Email: </label>
-                    <input type="email" name="emailParceiro" id="emailParceiro" placeholder="Digite o seu email."/>
-                </div>
-        </div>
-
-                    <input className="btn" type="button" value="Cadastrar" id="btnRegister"/>
-
-                <div className="login-data">
-                    <div className="login">
-                        <label htmlFor="loginParceiro">Digite seu login: </label>
-                        <input type="text" name="loginRegister" id="loginRegister" placeholder="Login"/>
-                    </div>
-
-                <div className="password-data">
-                    <label htmlFor="senhaParceiro">Digite sua senha: </label>
-                    <input type="password" name="senhaParceiro" id="senhaParceiro" placeholder="Senha"/>
-                </div>
-            </div>
-                    <input className="btn" type="button" value="Entrar" id="btnLogin"/>
-            </form>
+                  </div>
+                </li>
+              </ul>
+            </nav>
+          </form>
         </section>
-    </div>
+      </div>
+    )
+  }
+}
 
-        )
-    }
-};
-
-
-export default form;
+export default form
