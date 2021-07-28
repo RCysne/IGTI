43208e107642fb4fed0434e73d26943a167680c8
Ronaldo Cysne <ronaldocysne@gmail.com>
2021-07-28 17:43:21 -0300

    Calculadora IMC React
    Atividade Prévia Calculadora IMC React

---
+++ atividadePrevia6/src/Components/Header.jsx
@@ -0,0 +1,9 @@
+export default function Header() {
+  return (
+    <div className="header">
+      <h1>Calculadora IMC</h1>
+      <h2>Aluno: Ronaldo Cysne de Medeiros Vasconcelos</h2>
+    </div>
+  )
+}
+
